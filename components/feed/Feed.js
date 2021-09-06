5593485bc4de5dc74f92ca9f15f71c4f25e36dfd
Robert Cloa <42529441+8luM3CT0@users.noreply.github.com>
2021-09-06 16:45:32 -0700

    Updated Feed
    - Added the post box here for mobile responsiveness

--- components/feed/Feed.js
+++ components/feed/Feed.js
@@ -1,10 +1,10 @@
 //front-end
 import { HomeIcon } from '@heroicons/react/outline'
 import Post from './post/Post'
+import PostBox from '../sidebar/post-box/PostBox'
 //back-end
 import { useEffect, useState } from 'react'
 import { store } from '../../firebase-file/firebaseFile'
-import { PostAddRounded } from '@material-ui/icons'
 
 function Feed () {
   const [posts, setPosts] = useState([])
@@ -36,6 +36,9 @@ function Feed () {
       >
         <h2 className='text-xl text-gray-300 font-semibold'>Home</h2>
         <HomeIcon className='text-gray-300 h-10' />
+      </div>
+      <div className='md:hidden mt-4'>
+        <PostBox />
       </div>
       <div className='overflow-y-scroll scrollbar-hide flex-1'>
         {posts.map(post => (
